e215507c93b3542ace07653f7047b4855a674049
NH Shakil <nhshakil@NHs-MacBook-Air.local>
2026-04-29 22:33:59 +0600

    variable-javascipt

---
+++ hello.js
@@ -0,0 +1,67 @@
+// Variable var,let,const
+/*
+let phoneNumber = '01643894554';
+
+ a = 80;
+ b = 70;
+
+console.log(phoneNumber,a,b); 
+
+var Name = 'Shakil';
+
+console.log(Name);
+
+const ShakilName = 'NH Shakil';
+
+console.log(ShakilName);
+
+const person = {
+    fristName: 'Shakil',
+    age: '24',
+}
+person.country = 'Bangladesh',
+person.age = '33',
+
+console.log(person);
+
+*/
+
+//javascipt Operators
+
+/*
+* Arithmemtic Operator
+
+*/
+
+let a = 50;
+let b = 20;
+let c = a % b;
+
+console.log(c);
+/*
+let userInput = prompt('Enter a Number');
+let number = parseInt(userInput);
+
+if (number % 2 === 0){
+    console.log(`${number} is even number`);
+
+    }
+else {
+    console.log(`${number} is odd number `)
+}
+    */
+
+let x =10;
+x++; //inctrement
+console.log(`incremented ${x}`);
+
+
+let y =20;
+y = y -1;
+y--; //decrement
+
+console.log(`decremented ${y}`);
+
+
+
+
