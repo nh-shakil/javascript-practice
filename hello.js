005e0f3b095ec4c23afcfe1a64e1d56de6f71fa8
NH Shakil <nhshakil@NHs-MacBook-Air.local>
2026-05-02 01:00:26 +0600

    operators-precedence

--- hello.js
+++ hello.js
@@ -161,6 +161,22 @@ console.log(broName);
 console.log(typeof broName);
 
 
+//javascipt Operators Precedence 
+
+// ===== () , {}, []
+
+// / , +, -, * 
+
+/*
+  5 + 6 - 3 / 2 * 4
+  =5 + 6 - 1.5 * 4
+  =5 + 6 - 6 
+  =5
+*/
+
+console.log(5 + 6 - 3 / 2 * 4); //  ans 5 
+
+
 
 
 
